10a22b9322697b8ac2945d9e9f747299b821960b
htdong <donght@shinemo.com>
2018-04-20 16:29:07 +0800

    c

--- _Main/src/com/htdong/codeforces/Main.java
+++ _Main/src/com/htdong/codeforces/Main.java
@@ -12,7 +12,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        System.out.println(n / 2 + 1);
+        while (n > 0) {
+            --n;
+            String s = in.next();
+            if (s.length() <= 10) {
+                System.out.println(s);
+            } else {
+                System.out.println(s.charAt(0) + "" + (s.length() - 2) + s.charAt(s.length() - 1));
+            }
+        }
         in.close();
     }
 }
