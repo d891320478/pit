272579b452f6803156255442a1c500c421b66696
htdong <donght@shinemo.com>
2018-05-08 20:03:43 +0800

    c

--- _Main/src/com/htdong/codeforces/Main.java
+++ _Main/src/com/htdong/codeforces/Main.java
@@ -1,5 +1,7 @@
 package com.htdong.codeforces;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -9,17 +11,31 @@ import java.util.Scanner;
  */
 
 public class Main {
+
+    private static boolean[] flag;
+    private static int ans;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        while (n > 0) {
-            --n;
-            String s = in.next();
-            if (s.length() <= 10) {
-                System.out.println(s);
-            } else {
-                System.out.println(s.charAt(0) + "" + (s.length() - 2) + s.charAt(s.length() - 1));
+        int m = in.nextInt();
+        List<List<Integer>> e = new ArrayList<>(n + 2);
+        for (int i = 0; i < m; ++i) {
+            int u = in.nextInt();
+            int v = in.nextInt();
+            if (e.get(u) == null) {
+                e.set(u, new ArrayList<>());
             }
+            if (e.get(v) == null) {
+                e.set(v, new ArrayList<>());
+            }
+            e.get(u).add(v);
+            e.get(v).add(u);
+        }
+        flag = new boolean[n + 2];
+        ans = 0;
+        for (int i = 1; i <= n; ++i) {
+            flag[i] = true;
         }
         in.close();
     }
