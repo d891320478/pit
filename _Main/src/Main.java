5a28ffc756daea7fee9baf83ff506674352156b8
htdong <donght@shinemo.com>
2018-05-16 17:30:07 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -7,12 +7,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Integer a = 1;
-        Integer b = 10000;
-        Integer c = null;
-        System.out.println(a == 1);
-        System.out.println(b == 10000);
-        System.out.println(c == 0);
+        String s = "bcdefbcd";
+        s = s.replaceAll("cd", "aaaa");
+        System.out.println(s);
     }
 
 }
