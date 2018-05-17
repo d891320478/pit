1cdb2b4ba5580645f46b2097c6cc0e9a350e937c
htdong <donght@shinemo.com>
2018-05-17 16:20:33 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -6,10 +6,21 @@ import java.io.IOException;
  */
 public class Main {
 
+    interface Test {
+        boolean check(int x);
+    }
+
+    private static void print(int[] a, Test b) {
+        for (int i : a) {
+            if (b.check(i)) {
+                System.err.println(i);
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        String s = "bcdefbcd";
-        s = s.replaceAll("cd", "aaaa");
-        System.out.println(s);
+        int[] a = { 1, 2, 3, 4, 5 };
+        print(a, (int x) -> x > 2);
     }
 
 }
