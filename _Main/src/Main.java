3eafa0f5912316b3c36514b4d00ffb1cf2e9c575
htdong <donght@shinemo.com>
2018-06-19 15:28:56 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -6,21 +6,8 @@ import java.io.IOException;
  */
 public class Main {
 
-    interface Test {
-        boolean check(int x);
-    }
-
-    private static void print(int[] a, Test b) {
-        for (int i : a) {
-            if (b.check(i)) {
-                System.err.println(i);
-            }
-        }
-    }
-
     public static void main(String[] args) throws IOException {
-        int[] a = { 1, 2, 3, 4, 5 };
-        print(a, (int x) -> x > 2);
+        System.out.println(Math.ceil(4.0001));
     }
 
 }
