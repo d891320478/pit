11142792f9ba2e5eb53a181882ce54d19322aef0
htdong <donght@shinemo.com>
2018-07-11 21:08:01 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,14 +1,33 @@
 import java.io.IOException;
-import java.util.Date;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 
  * @author htdong
  */
+
+class A {
+    static int x = 1;
+    int a;
+
+    public A() {
+        this.a = ++A.x;
+    }
+
+    public int get() {
+        return a;
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(new Date(1530201600000L));
+        List<A> list = Arrays.asList(new A(), new A(), new A(), new A(), new A());
+        Integer[] s = list.stream().map(A::get).toArray(Integer[]::new);
+        for (Integer i : s) {
+            System.out.println(i);
+        }
     }
 
 }
