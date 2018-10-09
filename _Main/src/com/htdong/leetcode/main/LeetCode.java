569393805e77396ae5bff6459dd84361afa0047b
htdong <donght@shinemo.com>
2018-10-09 17:05:35 +0800

    c

--- _Main/src/com/htdong/leetcode/main/LeetCode.java
+++ _Main/src/com/htdong/leetcode/main/LeetCode.java
@@ -1,6 +1,6 @@
 package com.htdong.leetcode.main;
 
-import com.htdong.leetcode.solution.TopVotedCandidate;
+import com.htdong.leetcode.solution.Solution;
 
 /**
  * 
@@ -10,8 +10,8 @@ import com.htdong.leetcode.solution.TopVotedCandidate;
 
 public class LeetCode {
     public static void main(String[] args) {
-        int p[] = { 0, 0, 1, 1, 2 };
-        int t[] = { 0, 67, 69, 74, 87 };
-        new TopVotedCandidate(p, t);
+        String[] a = { "amazon", "apple", "facebook", "google", "leetcode" };
+        String[] b = { "e", "o" };
+        System.out.println(new Solution().wordSubsets(a, b));
     }
 }
--- _Main/src/com/htdong/leetcode/solution/Solution.java
+++ _Main/src/com/htdong/leetcode/solution/Solution.java
@@ -1,6 +1,7 @@
 package com.htdong.leetcode.solution;
 
-import com.htdong.leetcode.domain.TreeNode;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -9,34 +10,84 @@ import com.htdong.leetcode.domain.TreeNode;
  */
 
 public class Solution {
-    public TreeNode constructFromPrePost(int[] pre, int[] post) {
-        if (pre.length == 0) {
-            return null;
-        }
-        TreeNode rt = new TreeNode(pre[0]);
-        if (pre.length == 1) {
-            return rt;
-        }
-        int i = 0;
-        for (; post[i] != pre[1]; ++i)
-            ;
-        int[] pr1 = new int[i + 1];
-        int[] po1 = new int[i + 1];
-        int[] pr2 = new int[pre.length - 2 - i];
-        int[] po2 = new int[pre.length - 2 - i];
-        for (int i1 = 0, i2 = 0, z = 0; z < pre.length - 1; ++z) {
-            if (z <= i) {
-                pr1[i1] = pre[z + 1];
-                po1[i1] = post[z];
-                ++i1;
-            } else {
-                pr2[i2] = pre[z + 1];
-                po2[i2] = post[z];
-                ++i2;
+
+    int[] tr;
+
+    public int partitionDisjoint(int[] a) {
+        int n = a.length;
+        tr = new int[n * 2 + 5];
+        build(0, n - 1, a);
+        int mx = a[0];
+        for (int i = 1; i < n; ++i) {
+            if (mx <= query(0, n - 1, i, n - 1)) {
+                return i;
+            }
+            if (a[i] > mx) {
+                mx = a[i];
             }
         }
-        rt.left = constructFromPrePost(pr1, po1);
-        rt.right = constructFromPrePost(pr2, po2);
-        return rt;
+        return n;
+    }
+
+    private int query(int l, int r, int s, int t) {
+        if (s <= l && t >= r) {
+            return tr[idx(l, r)];
+        }
+        int mid = l + r >> 1;
+        if (s <= mid && t > mid) {
+            return Math.min(query(l, mid, s, t), query(mid + 1, r, s, t));
+        } else if (s <= mid) {
+            return query(l, mid, s, t);
+        } else {
+            return query(mid + 1, r, s, t);
+        }
+    }
+
+    private void build(int l, int r, int[] a) {
+        if (l == r) {
+            tr[idx(l, r)] = a[l];
+            return;
+        }
+        int mid = l + r >> 1;
+        build(l, mid, a);
+        build(mid + 1, r, a);
+        tr[idx(l, r)] = Math.min(tr[idx(l, mid)], tr[idx(mid + 1, r)]);
+    }
+
+    private int idx(int l, int r) {
+        return (l + r) | (l != r ? 1 : 0);
+    }
+
+    public List<String> wordSubsets(String[] a, String[] b) {
+        List<String> rlt = new LinkedList<>();
+        int[] c1 = new int[26];
+        for (int i = 0; i < b.length; ++i) {
+            int[] c2 = new int[26];
+            for (int j = 0; j < b[i].length(); ++j) {
+                ++c2[b[i].charAt(j) - 'a'];
+            }
+            for (int j = 0; j < 26; ++j) {
+                if (c1[j] < c2[j]) {
+                    c1[j] = c2[j];
+                }
+            }
+        }
+        for (int i = 0; i < a.length; ++i) {
+            int[] c2 = new int[26];
+            for (int j = 0; j < a[i].length(); ++j) {
+                ++c2[a[i].charAt(j) - 'a'];
+            }
+            boolean flag = true;
+            for (int j = 0; j < 26; ++j) {
+                if (c2[j] < c1[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                rlt.add(a[i]);
+            }
+        }
+        return rlt;
     }
 }
