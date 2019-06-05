062ea38eecc684927bcdd755bc290f60bad1680e
htdong <donght@shinemo.com>
2019-06-05 16:31:53 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,16 +1,57 @@
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * 
  * @author htdong
  */
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(LocalDateTime.ofInstant(new Date(1558253324000L).toInstant(), ZoneId.systemDefault()));
+        ConditionTest test = new ConditionTest();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                test.funcA();
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                test.funcB();
+            }
+        }).start();
+    }
+}
+
+class ConditionTest {
+    private ReentrantLock lock;
+    private Condition cond;
+    
+    public ConditionTest() {
+        lock = new ReentrantLock();
+        cond = lock.newCondition();
+    }
+    
+    public void funcA() {
+        lock.lock();
+        try {
+            cond.await();
+            System.out.println("funcA");
+        } catch (InterruptedException e) {
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    public void funcB() {
+        lock.lock();
+        try {
+            System.out.println("funcB");
+            cond.signal();
+        } finally {
+            lock.unlock();
+        }
     }
 }
--- _Main/src/com/htdong/leetcode/main/LeetCode.java
+++ _Main/src/com/htdong/leetcode/main/LeetCode.java
@@ -3,13 +3,12 @@ package com.htdong.leetcode.main;
 import com.htdong.leetcode.solution.Solution;
 
 /**
- * 
  * @author htdong
  * @date 2018年4月8日 下午2:36:19
  */
 
 public class LeetCode {
     public static void main(String[] args) {
-        System.out.println(new Solution().lastStoneWeightII(new int[] { 2, 7, 4, 1, 8, 1 }));
+        System.out.println(new Solution());
     }
 }
