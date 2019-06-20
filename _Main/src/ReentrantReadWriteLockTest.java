3bf5bc60b6bb9ee54c4a29264f524a823b1d230c
htdong <donght@shinemo.com>
2019-06-20 14:49:42 +0800

    c

--- _Main/src/ReentrantReadWriteLockTest.java
+++ _Main/src/ReentrantReadWriteLockTest.java
@@ -1,5 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class ReentrantReadWriteLockTest {
     public static void main(String[] args) {
+        final Map<String, String> map = new HashMap<>();
+        final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+        final String key = "key";
+        for (int i = 0; i < 4; ++i) {
+            final int thr = i;
+            new Thread(new Runnable() {
 
+                @Override
+                public void run() {
+                    lock.readLock().lock();
+                    try {
+                        String val = map.get(key);
+                        if (val == null) {
+                            lock.readLock().unlock();
+                            lock.writeLock().lock();
+                            try {
+                                val = map.get(key);
+                                if (val == null) {
+                                    map.put(key, thr + "");
+                                    System.out.println("=== " + thr);
+                                }
+                                lock.readLock().lock();
+                            } finally {
+                                lock.writeLock().unlock();
+                            }
+                        }
+                    } finally {
+                        lock.readLock().unlock();
+                    }
+                }
+            }).start();
+        }
+        System.err.println(map.get(key));
     }
 }
--- _Main/src/StampedLockTest.java
+++ _Main/src/StampedLockTest.java
@@ -1,5 +1,7 @@
+import java.util.concurrent.locks.StampedLock;
+
 public class StampedLockTest {
     public static void main(String[] args) {
-        
+        StampedLock lock = new StampedLock();
     }
 }
