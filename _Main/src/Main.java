d511e4526517f24c97623a9035d0f4775e7446df
htdong <donght@shinemo.com>
2019-06-25 10:00:23 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author htdong
@@ -7,7 +9,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("E:\\static-world-logo.png");
-        System.out.println(file.length());
+        Pattern p = Pattern.compile("共\\d+页");
+        Pattern p1 = Pattern.compile("\\d+");
+        Matcher m = p.matcher("共18页");
+        m.find();
+        String b = m.group();
+        Matcher m1 = p1.matcher(b);
+        m1.find();
+        int a = Integer.parseInt(m1.group());
+        System.out.println(a);
     }
 }
