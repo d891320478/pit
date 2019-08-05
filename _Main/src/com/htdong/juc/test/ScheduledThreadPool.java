931af8c18921f801da8c623186cc9bfb218c8cef
htdong <donght@shinemo.com>
2019-08-05 15:46:58 +0800

    c

--- _Main/src/com/htdong/juc/test/ScheduledThreadPool.java
+++ _Main/src/com/htdong/juc/test/ScheduledThreadPool.java
@@ -1,11 +1,13 @@
 package com.htdong.juc.test;
 
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
 /**
  * @author htdong
  * @date 2019年8月2日 下午5:22:26
  */
 public class ScheduledThreadPool {
     public static void main(String[] args) {
-
+        ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(1);
     }
 }
