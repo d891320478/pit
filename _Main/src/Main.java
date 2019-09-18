ba6c9b2ba96646db2a75a042dfd1fff0a9ce08ab
htdong <donght@shinemo.com>
2019-09-18 15:35:30 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -8,17 +8,7 @@ import java.util.PriorityQueue;
 public class Main {
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> q = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2.compareTo(o1);
-            }
-        });
-        for (int i = 0; i < 10; ++i) {
-            q.add(i);
-        }
-        while (!q.isEmpty()) {
-            System.out.println(q.poll());
-        }
+        String s = "我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我我";
+        System.out.println(s.length());
     }
 }
--- _Main/src/com/htdong/leetcode/domain/Node.java
+++ _Main/src/com/htdong/leetcode/domain/Node.java
@@ -1,22 +1,21 @@
 package com.htdong.leetcode.domain;
 
-import java.util.List;
-
 /**
- *
  * @author htdong
  * @date 2018年8月1日 下午3:35:04
  */
 
 public class Node {
     public int val;
-    public List<Node> children;
+    public Node next;
+    public Node random;
 
     public Node() {
     }
 
-    public Node(int _val, List<Node> _children) {
+    public Node(int _val, Node _next, Node _random) {
         val = _val;
-        children = _children;
+        next = _next;
+        random = _random;
     }
 }
