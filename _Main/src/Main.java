7a8a28be78c358960f8a58b98f7a38c97cae57a2
htdong <donght@shinemo.com>
2019-11-17 17:30:50 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,12 +1,39 @@
-import java.util.Date;
-
 /**
  * @author htdong
  */
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(new Date(1573181137819L));
+    static class C {
+        public int n;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        C c = new C();
+        C d = new C();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (c) {
+                    c.n = 100;
+                    d.n = 100;
+                    while (!Thread.interrupted()) {
+                    }
+                    System.out.println(Thread.currentThread().getName());
+                }
+            }
+        });
+        t1.start();
+        Thread t2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " " + d.n + " " + c.n);
+            }
+        });
+        t2.start();
+        t2.join();
+        t1.interrupt();
+        c.n = 101 * (int) System.currentTimeMillis();
     }
 }
