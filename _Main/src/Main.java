8325f12477116aeb243e7fc22a4f4ac391fd56ef
htdong <donght@shinemo.com>
2019-11-27 23:12:11 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,25 +1,19 @@
-import java.lang.ref.ReferenceQueue;
-import java.lang.ref.WeakReference;
-import java.time.LocalDate;
-
 /**
  * @author htdong
  */
 
 public class Main {
 
-    public static void main(String[] args) {
-        String a = LocalDate.now().toString();
-        ReferenceQueue<String> rq = new ReferenceQueue<>();
-        WeakReference<String> wr = new WeakReference<String>(a, rq);
-        System.out.println(a);
-        a = null;
-        System.out.println(wr.get());
-        System.out.println(rq.poll());
-        System.gc();
-        System.gc();
-        System.gc();
-        System.out.println(wr.get());
-        System.out.println(rq.poll());
+    public static void main(String[] args) throws InterruptedException {
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                System.err.println(Thread.currentThread().getId());
+            }
+        };
+        Thread t = new Thread(run);
+        t.start();
+        t.join();
+        t.start();
     }
 }
