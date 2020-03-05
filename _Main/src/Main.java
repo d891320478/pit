a0d14bfdf8e49d9959cf8af9e48d9d08604f8742
htdong <donght@shinemo.com>
2020-03-05 14:42:13 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -4,11 +4,6 @@
 
 public class Main {
 
-    private static final String PARAM_TIMESTAMP = "timestamp";
-
-    public static void main(String[] args) throws InterruptedException {
-        String[] ss = "epidemic-admin?backDoor?".split("\\?");
-        ss[1] = PARAM_TIMESTAMP + "=" + System.currentTimeMillis() + (ss[1] == null ? "" : "&" + ss[1]);
-        System.out.println(String.join("?", ss));
+    public static void main(String[] args) {
     }
 }
