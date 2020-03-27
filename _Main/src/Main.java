cddb5209b12bcb084ce26df4e18c98efcb98985d
htdong <donght@shinemo.com>
2020-03-27 10:30:24 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,7 +1,3 @@
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineFactory;
-import javax.script.ScriptEngineManager;
-
 /**
  * @author htdong
  */
@@ -9,13 +5,7 @@ import javax.script.ScriptEngineManager;
 public class Main {
 
     public static void main(String[] args) {
-        final ScriptEngineManager mgr = new ScriptEngineManager();
-        ScriptEngine engine = mgr.getEngineByName("groovy");
-        System.err.println(engine == null);
-        for (ScriptEngineFactory fac : mgr.getEngineFactories()) {
-            System.out.println(String.format("%s (%s), %s (%s), %s", fac.getEngineName(), fac.getEngineVersion(),
-                    fac.getLanguageName(), fac.getLanguageVersion(), fac.getParameter("THREADING")));
-        }
+        System.out.println(System.currentTimeMillis());
     }
 
 }
