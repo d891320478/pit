6bd3ba7f151bd04f8961e543efa5428130508de7
htdong <donght@shinemo.com>
2021-01-07 14:49:33 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,4 +1,7 @@
-import java.time.LocalDate;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * @author htdong
@@ -6,8 +9,59 @@ import java.time.LocalDate;
 
 public class Main {
 
-    public static void main(String[] args) {
-        LocalDate t = LocalDate.of(2020, 0, 1);
-        System.out.println(t);
+    public static void main(String[] args) throws IOException {
+        FileWriter outMonth = new FileWriter(new File("E:/login_month.txt"), true);
+        FileWriter outWeek = new FileWriter(new File("E:/login_week.txt"), true);
+        FileWriter outDay = new FileWriter(new File("E:/login_day.txt"), true);
+        File[] file = new File[] { new File("E:/infolog-cmmc1"), new File("E:/infolog-cmmc2") };
+        for (File iter : file) {
+            File[] ff = iter.listFiles();
+            for (File jter : ff) {
+                Scanner in = new Scanner(jter);
+                while (in.hasNextLine()) {
+                    String s = in.nextLine();
+                    if (!s.contains("[handleUserInfoResult] 获取解密后的用户信息")) {
+                        continue;
+                    }
+                    System.out.println(s);
+                    String date = s.substring(0, 19);
+                    String mobile = null;
+                    if (s.contains("\"phone\":\"")) {
+                        mobile = s.substring(s.indexOf("\"phone\":\"") + 9, s.indexOf("\"phone\":\"") + 20);
+                    }
+                    String city = null;
+                    if (s.contains("\"city\":\"")) {
+                        city = s.substring(s.indexOf("\"city\":\"") + 8, s.indexOf("\"city\":\"") + 28);
+                        city = city.substring(0, city.indexOf("\""));
+                    } else if (s.contains("\"cityName\":\"")) {
+                        city = s.substring(s.indexOf("\"cityName\":\"") + 12, s.indexOf("\"cityName\":\"") + 32);
+                        city = city.substring(0, city.indexOf("\""));
+                    }
+                    String name = null;
+                    if (s.contains("\"name\":\"")) {
+                        name = s.substring(s.indexOf("\"name\":\"") + 8, s.indexOf("\"name\":\"") + 18);
+                        name = name.substring(0, name.indexOf("\""));
+                    }
+                    if(mobile == null && name == null && city == null) {
+                        continue;
+                    }
+                    if (s.startsWith("2021-01-06")) {
+                        outDay.write(date + " " + city + " " + mobile + " " + name + "\r\n");
+                        outWeek.write(date + " " + city + " " + mobile + " " + name + "\r\n");
+                    }
+                    if (s.startsWith("2021-01-04") || s.startsWith("2021-01-05")) {
+                        outWeek.write(date + " " + city + " " + mobile + " " + name + "\r\n");
+                    }
+                    outMonth.write(date + " " + city + " " + mobile + " " + name + "\r\n");
+                }
+                in.close();
+            }
+        }
+        outDay.flush();
+        outDay.close();
+        outWeek.flush();
+        outWeek.close();
+        outMonth.flush();
+        outMonth.close();
     }
 }
--- _Main/src/com/htdong/sgu/Main.java
+++ _Main/src/com/htdong/sgu/Main.java
@@ -1,6 +1,8 @@
 package com.htdong.sgu;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * @author htdong
@@ -8,8 +10,33 @@ import java.util.Scanner;
  */
 public class Main {
 
+    static class Fastget {
+
+        public BufferedReader in;
+        public StringTokenizer st;
+
+        public Fastget() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(in.readLine());
+                } catch (Exception e) {
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            String s = next();
+            return Integer.parseInt(s);
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        in.close();
+        // Scanner in = new Scanner(System.in);
+        // in.close();
     }
 }
