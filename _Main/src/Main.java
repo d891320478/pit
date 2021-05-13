4d6365cae2b465c92f156dff805357a60cd3e211
htdong <donght@shinemo.com>
2021-05-13 15:21:09 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,58 +1,13 @@
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import java.util.Date;
 
 /**
  * @author htdong
  */
 
 public class Main {
-
-    public static String getSignature(String message, String secret, String hmacAlgorithm) {
-        String signature = null;
-        try {
-            Mac mac = Mac.getInstance(hmacAlgorithm);
-            SecretKeySpec secretKeySpec = null;
-            try {
-                secretKeySpec = new SecretKeySpec(secret.getBytes("UTF-8"), hmacAlgorithm);
-                mac.init(secretKeySpec);
-                BASE64Encoder base64 = new BASE64Encoder();
-                signature = base64.encode(mac.doFinal(message.getBytes("UTF-8")));
-            } catch (UnsupportedEncodingException e) {
-            }
-        } catch (InvalidKeyException | NoSuchAlgorithmException e) {
-        }
-        return signature;
-    }
-
-    public static String getSignature(String message) {
-        String signature = "";
-        // 签名算法
-        String algorithmForMac = "HmacSHA256";
-        // 子账号秘钥
-        String secret = "UnNDditTVytPdG5ib2VkMzl1YXlOQT09";
-        BASE64Decoder decoder = new BASE64Decoder();
-        try {
-            // base64解密
-            secret = new String(decoder.decodeBuffer(secret), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        signature = getSignature(message, secret, algorithmForMac);
-        return signature;
-    }
-
     public static void main(String[] args) throws UnsupportedEncodingException, IOException {
-        String msg = "requestRefId=jxwangge1616983627094&secretId=979l8R2RX3418Gi10uYDS&x-date=Mon, 29 Mar 2021 02:07:07 GMT";
-        System.out.println(getSignature(msg));
+        System.out.println(new Date(1619666078261L));
     }
 }
--- _MainMaven/src/main/java/Main.java
+++ _MainMaven/src/main/java/Main.java
@@ -1,12 +1,4 @@
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.json.JsonReadFeature;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
-import lombok.Data;
+import com.shinemo.client.util.AESUtil;
 
 /**
  * @author htdong
@@ -14,21 +6,7 @@ import lombok.Data;
 
 public class Main {
 
-    @Data
-    public static class Test {
-        private @JsonProperty("isTest") boolean isTest;
-    }
-
-    public static void main(String[] args) throws JsonProcessingException {
-        String json = "{\"isTest\":true}";
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.enable(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature());
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        Test test = objectMapper.readValue(json, Test.class);
-        System.out.println(test.isTest);
+    public static void main(String[] args) {
+        System.out.println(AESUtil.generateSeed());
     }
 }
