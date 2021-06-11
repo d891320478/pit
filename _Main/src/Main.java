4e51ec64a098d8fcee7df287a7189b05692265c4
htdong <donght@shinemo.com>
2021-06-11 11:42:32 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -7,7 +7,17 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner in = new Scanner(System.in);
+    private static int[][][][][][] vis = new int[7][7][7][7][7][7];
+    private static int[] a = new int[6];
 
     public static void main(String[] args) {
+        int _t = in.nextInt();
+        while (_t-- > 0) {
+            int n = in.nextInt();
+            for (int i = 0; i < 6; ++i) {
+                a[i] = i < n ? in.nextInt() : 0;
+            }
+            System.out.println(vis[a[0]][a[1]][a[2]][a[3]][a[4]][a[5]] == 1 ? "orzwym6912" : "orzwang9897");
+        }
     }
 }
--- _MainMaven/src/main/java/Main.java
+++ _MainMaven/src/main/java/Main.java
@@ -1,4 +1,4 @@
-import com.shinemo.client.util.AESUtil;
+import com.shinemo.net.bridge.server.util.TokenAuthenticator;
 
 /**
  * @author htdong
@@ -7,6 +7,9 @@ import com.shinemo.client.util.AESUtil;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(AESUtil.generateSeed());
+        String keyPath = "D:/data/";
+        TokenAuthenticator.initKeyPath(keyPath);
+        System.out.println(TokenAuthenticator.check(
+                "047DF7586AD3701CF6D3FA7875C4E14CE4DF49AAD9F73D14E369EA8CD8BF17EA1881FAF5CADD544FEB1BA3B099AB8628D45680BCB90F45C492EF637DCCE4FB1D7532AB7ADF853846CC222531BBBD0910F7082900D44FDADEFB5949FF7D6A0C06AC4B43F93163B7873919C9842F0595FDCF430E55D4D3BCF2F2807944C415F1C675174D2AEE2959"));
     }
 }
