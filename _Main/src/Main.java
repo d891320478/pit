c871729c6ce5a04f4ba14106104594130eb59581
htdong <donght@shinemo.com>
2021-07-06 20:06:28 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -9,14 +9,21 @@ public class Main {
     private static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n = in.nextInt();
-        int sum = 0;
-        int mut = 1;
-        for (int i = 0; i < n; ++i) {
-            int a = in.nextInt();
-            sum = ((sum + a) % 2 + 2) % 2;
-            mut = (mut * a % 2 + 2) % 2;
-            System.out.printf("%d%d\n", sum, mut);
+    }
+
+    private Object lock = new Object();
+
+    public void f1() throws InterruptedException {
+        synchronized (lock) {
+            lock.wait();
+            System.out.println("1");
+        }
+    }
+
+    public void f2() {
+        synchronized (lock) {
+            lock.notifyAll();
+            System.out.println("2");
         }
     }
 }
