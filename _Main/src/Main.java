7cf16fc3f7ffdb3275dd2c3afc2dc80671d0b18a
htdong <donght@shinemo.com>
2021-07-12 11:29:49 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,26 +1,29 @@
-import java.util.Scanner;
-
 /**
  * @author htdong
  */
 
 public class Main {
 
-    private static Scanner in = new Scanner(System.in);
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(Main::f1).start();
+        Thread.sleep(1000);
+        new Thread(Main::f2).start();
     }
 
-    private Object lock = new Object();
+    private static Object lock = new Object();
 
-    public void f1() throws InterruptedException {
+    public static void f1() {
         synchronized (lock) {
-            lock.wait();
-            System.out.println("1");
+            try {
+                lock.wait();
+                System.out.println("1");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public void f2() {
+    public static void f2() {
         synchronized (lock) {
             lock.notifyAll();
             System.out.println("2");
