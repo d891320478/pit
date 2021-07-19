286d123c1c0785b248902263d0adb83f0ed8d895
usr <dht31261@gmail.com>
2021-07-19 15:37:37 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,32 +1,13 @@
+import java.time.LocalTime;
+
 /**
  * @author htdong
  */
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        new Thread(Main::f1).start();
-        Thread.sleep(1000);
-        new Thread(Main::f2).start();
-    }
-
-    private static Object lock = new Object();
-
-    public static void f1() {
-        synchronized (lock) {
-            try {
-                lock.wait();
-                System.out.println("1");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static void f2() {
-        synchronized (lock) {
-            lock.notifyAll();
-            System.out.println("2");
-        }
-    }
+	public static void main(String[] args) throws InterruptedException {
+		int hour = LocalTime.now().getHour();
+		System.out.println(hour);
+	}
 }
