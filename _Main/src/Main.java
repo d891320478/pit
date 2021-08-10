0c567152630512e4c7faf027001e94b8a3084b66
usr <dht31261@gmail.com>
2021-08-10 20:31:52 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,37 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author htdong
  */
 
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException {
-		Thread t1 = new Thread(Main::f1);
-		t1.start();
-		Thread t2 = new Thread(Main::f2);
-		t2.start();
-		t1.interrupt();
-	}
-	
-	public static Object lock = new Object();
-	
-	public static void f1() {
-		synchronized(lock) {
-			try {
-				lock.wait();
-			} catch(Throwable e) {
-				System.out.println("f1 e");
-			}
-			System.out.println("f1 end");
-		}
-	}
-	
-	public static void f2() {
-		synchronized(lock) {
-			System.out.println("f2 start");
-			for(long i=0;i<10000000000L;++i) {
-			}
-			lock.notifyAll();
-			System.out.println("f2 end");
-		}
-	}
+    public static void main(String[] args) {
+        List<String> l = Arrays.asList("1", "2");
+        System.out.println(l.toArray(new String[0]).length);
+    }
 }
--- _MainMaven/src/main/java/Main.java
+++ _MainMaven/src/main/java/Main.java
@@ -1,11 +1,6 @@
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
-import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * @author htdong
@@ -14,27 +9,8 @@ import org.apache.commons.codec.binary.Base64;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        URL url = new URL(
-                "http://10.0.19.105:8082/repository/basic/basic/server/dev-ops/202106291512/dev-ops-202106291512.jar");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        String basicAuth = "Basic " + Base64.encodeBase64String("deploy:deploy".getBytes());
-        connection.setRequestProperty("Authorization", basicAuth);
-        connection.setRequestProperty("Accept", "text/html");
-        connection.setRequestProperty("Accept-Language", "en-US");
-        connection.setRequestMethod("GET");
-        connection.setDoInput(true);
-        connection.setDoOutput(true);
-        String[] paths = url.getFile().split("/");
-        String fileName = paths[paths.length - 1];
-        File f = new File("E:/" + fileName);
-        try (BufferedInputStream bis = new BufferedInputStream(connection.getInputStream());
-                FileOutputStream fis = new FileOutputStream(f);) {
-            byte[] buffer = new byte[1024];
-            int count = 0;
-            while ((count = bis.read(buffer, 0, 1024)) != -1) {
-                fis.write(buffer, 0, count);
-            }
-        }
-        System.out.println("finish");
+        String[] a = new String[] { "a", "b" };
+        String[] b = new String[] { "a", "b" };
+        System.out.println(ArrayUtils.addAll(a, b).length);
     }
 }
