30b88a67f6861668ec3e087262ebb88a5538de0d
usr <dht31261@gmail.com>
2021-08-19 15:41:56 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * @author htdong
  */
@@ -8,7 +5,5 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> l = Arrays.asList("1", "2");
-        System.out.println(l.toArray(new String[0]).length);
     }
 }
--- _MainMaven/src/main/java/Main.java
+++ _MainMaven/src/main/java/Main.java
@@ -1,6 +1,9 @@
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
-import org.apache.commons.lang3.ArrayUtils;
+import com.shinemo.client.common.lib.GsonUtil;
 
 /**
  * @author htdong
@@ -9,8 +12,20 @@ import org.apache.commons.lang3.ArrayUtils;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String[] a = new String[] { "a", "b" };
-        String[] b = new String[] { "a", "b" };
-        System.out.println(ArrayUtils.addAll(a, b).length);
+        File f = new File("/User/htdong/Desktop/config copy.js");
+        StringBuilder sb = new StringBuilder();
+        try (FileReader fin = new FileReader(f)) {
+            char[] b = new char[1024];
+            for (int n = fin.read(b); n != -1; n = fin.read(b)) {
+                sb.append(b);
+            }
+        }
+        List<Path> list = GsonUtil.fromJsonToList(sb.toString(), Path[].class);
+    }
+
+    public static class Path {
+        private String title;
+        private String path;
+        private List<Path> routes;
     }
 }
