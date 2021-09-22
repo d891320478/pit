f802ca9ac24fa4d25e66c93d21a98796bf387864
usr <dht31261@gmail.com>
2021-09-22 16:47:07 +0800

    c

--- _MainMaven/src/main/java/Main.java
+++ _MainMaven/src/main/java/Main.java
@@ -1,31 +1,15 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-
-import com.shinemo.client.common.lib.GsonUtil;
-
 /**
  * @author htdong
  */
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        File f = new File("/User/htdong/Desktop/config copy.js");
-        StringBuilder sb = new StringBuilder();
-        try (FileReader fin = new FileReader(f)) {
-            char[] b = new char[1024];
-            for (int n = fin.read(b); n != -1; n = fin.read(b)) {
-                sb.append(b);
-            }
+    public static void main(String[] args) {
+        String a = "我人有";
+        byte[] b = a.getBytes();
+        for (int i = 0; i < b.length; ++i) {
+            System.out.println(b[i]);
         }
-        List<Path> list = GsonUtil.fromJsonToList(sb.toString(), Path[].class);
     }
 
-    public static class Path {
-        private String title;
-        private String path;
-        private List<Path> routes;
-    }
 }
