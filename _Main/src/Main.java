15decbce39b61fb56d38931384c197aadc9fbb29
usr <dht31261@gmail.com>
2021-11-22 18:47:27 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -4,11 +4,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        String a = "a1b2c3d456fre ";
-        String[] b = a.replaceAll("[^0-9]", " ").split("\s+");
-        System.out.println(b[0]);
-        System.out.println(b[1]);
-        System.out.println(b[2]);
-        System.out.println(b[3]);
+        Integer a = Integer.valueOf("1234");
+        Integer b = Integer.valueOf("1234");
+        System.out.println(System.identityHashCode(a));
+        System.out.println(System.identityHashCode(b));
     }
 }
