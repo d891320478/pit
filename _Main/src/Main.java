c3f83fa245c24180ea0a2b7f0ff5144338dcd343
usr <dht31261@gmail.com>
2021-11-25 17:43:19 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -4,9 +4,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        Integer a = Integer.valueOf("1234");
-        Integer b = Integer.valueOf("1234");
-        System.out.println(System.identityHashCode(a));
-        System.out.println(System.identityHashCode(b));
+        System.out.println("123".indexOf(null));
     }
 }
--- _MainMaven/src/main/java/Main.java
+++ _MainMaven/src/main/java/Main.java
@@ -1,13 +1,39 @@
 import java.util.Date;
 
+import com.shinemo.jce.util.Container;
+
 /**
  * @author htdong
  */
 
 public class Main {
+    
+    public static class A {
+        private int a;
+        
+        public A(int a) {
+            this.a = a;
+        }
+        
+        public int getA() {
+            return a;
+        }
+    }
+    
+    public static class B extends A {
+
+        public B(int a) {
+            super(a);
+        }
+        
+    }
+    
+    static A a = new A(1);
+    static B b = new B(2);
 
     public static void main(String[] args) {
-        System.out.println(new Date(1632903670144L));
+        System.out.println(Container.get(A.class).getA());
+        System.out.println(Container.get(B.class).getA());
     }
 
 }
