68396c26acb9a37ae1e482b6895375c9d33a1f58
usr <dht31261@gmail.com>
2021-12-27 15:38:01 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,11 +1,10 @@
-import java.util.Date;
-
 /**
  * @author htdong
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new Date(1640078852827L));
+        String str = "${ip}${ip}${ip}";
+        System.out.println(str.replaceAll("\\$\\{ip\\}", "123"));
     }
 }
