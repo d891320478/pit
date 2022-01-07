e6cd65b19fcde6f4cc651fa36b69c89ce107a509
htdong <dht31261@gmail.com>
2022-01-07 10:09:49 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -4,7 +4,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        String str = "${ip}${ip}${ip}";
-        System.out.println(str.replaceAll("\\$\\{ip\\}", "123"));
+        String str = "/";
+        System.out.println(str.endsWith("/"));
     }
 }
