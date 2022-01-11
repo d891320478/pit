b10223cfc0cb3e37a8c165665ef35663bba8638e
htdong <dht31261@gmail.com>
2022-01-11 22:45:02 +0800

    c

--- _MainMaven/src/main/java/Main.java
+++ _MainMaven/src/main/java/Main.java
@@ -1,39 +1,8 @@
-import java.util.Date;
-
-import com.shinemo.jce.util.Container;
-
-/**
- * @author htdong
- */
 
 public class Main {
-    
-    public static class A {
-        private int a;
-        
-        public A(int a) {
-            this.a = a;
-        }
-        
-        public int getA() {
-            return a;
-        }
-    }
-    
-    public static class B extends A {
-
-        public B(int a) {
-            super(a);
-        }
-        
-    }
-    
-    static A a = new A(1);
-    static B b = new B(2);
 
     public static void main(String[] args) {
-        System.out.println(Container.get(A.class).getA());
-        System.out.println(Container.get(B.class).getA());
-    }
+        // TODO Auto-generated method stub
 
+    }
 }
