27bce2c157ba2ad3b5e89d627ecb34e9f445d523
htdong <dht31261@gmail.com>
2022-01-26 22:16:26 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,10 +1,13 @@
+import java.time.LocalDateTime;
+
 /**
  * @author htdong
  */
 public class Main {
 
     public static void main(String[] args) {
-        String str = "/";
-        System.out.println(str.endsWith("/"));
+        LocalDateTime now = LocalDateTime.now();
+        now.plusDays(-7);
+        System.out.println(now);
     }
 }
