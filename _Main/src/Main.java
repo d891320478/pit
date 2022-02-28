ec0df69f376803256bad8f0ebc6a3f4be04506b2
htdong <dht31261@gmail.com>
2022-02-28 15:05:48 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,13 +1,29 @@
-import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @author htdong
  */
 public class Main {
 
+    static class CompratorByFileName implements Comparator<String> {
+        public int compare(String f1, String f2) {
+            long diff = f2.compareTo(f1);
+            if (diff > 0) {
+                return 1;
+            } else if (diff < 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        LocalDateTime now = LocalDateTime.now();
-        now.plusDays(-7);
-        System.out.println(now);
+        String[] s = new String[] { "a", "b", "ca", "c", "d" };
+        Arrays.sort(s, new CompratorByFileName());
+        for (String i : s) {
+            System.out.println(i);
+        }
     }
 }
