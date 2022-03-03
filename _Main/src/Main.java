734f1db71b6cd8b3492eda7ce41bb2a00e92380f
htdong <dht31261@gmail.com>
2022-03-03 17:12:00 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,29 +1,19 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 /**
  * @author htdong
  */
 public class Main {
 
-    static class CompratorByFileName implements Comparator<String> {
-        public int compare(String f1, String f2) {
-            long diff = f2.compareTo(f1);
-            if (diff > 0) {
-                return 1;
-            } else if (diff < 0) {
-                return -1;
-            } else {
-                return 0;
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        String[] s = new String[] { "a", "b", "ca", "c", "d" };
-        Arrays.sort(s, new CompratorByFileName());
-        for (String i : s) {
-            System.out.println(i);
+        long[] uids = new long[] { 99812416L, 10101001231961896L, 10101001228401944L, 10101001231981344L, 106104L,
+                10101001229324392L, 10101001232145560L, 10101001231307328L, 10101001232668320L, 10101001232528920L };
+        long[] menus = new long[] { 344, 357, 359, 365, 371, 378, 379, 383, 391, 392, 396, 382, 384, 385, 387, 388 };
+        // String ins = "insert into user_menu (site_id, uid, app_id, menu_id, stat,
+        // gmt_create, kind) values (0, %s, 7, 377, 1, now(), 0);\n";
+        String sql = "insert into user_menu (site_id, uid, app_id, menu_id, stat, gmt_create, kind) values (0, %s, 7, %s, 1, now(), 0);\n";
+        for (long i : uids) {
+            for (long j : menus) {
+                System.out.printf(sql, i + "", j + "");
+            }
         }
     }
 }
