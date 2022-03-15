dc98c636946e7e63809198aa14fbe07226511e2c
htdong <dht31261@gmail.com>
2022-03-15 15:46:55 +0800

    c

--- _Main/src/com/htdong/leetcode/solution/LeetCode.java
+++ _Main/src/com/htdong/leetcode/solution/LeetCode.java
@@ -15,18 +15,17 @@ public class LeetCode {
 
         public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
 
-            // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+            // releaseHydrogen.run() outputs 'H'. Do not change or remove this line.
             releaseHydrogen.run();
         }
 
         public void oxygen(Runnable releaseOxygen) throws InterruptedException {
 
-            // releaseOxygen.run() outputs "O". Do not change or remove this line.
+            // releaseOxygen.run() outputs 'O'. Do not change or remove this line.
             releaseOxygen.run();
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution());
     }
 }
