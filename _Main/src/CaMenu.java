7038427506fc8f91ab239dd45a2b1ff214c25964
htdong <dht31261@gmail.com>
2022-03-17 14:08:06 +0800

    c

--- _Main/src/CaMenu.java
+++ _Main/src/CaMenu.java
@@ -7,8 +7,6 @@ public class CaMenu {
         long[] uids = new long[] { 10101001233619744L, 77530504L, 10101001230693160L, 10101001232946024L,
                 10101001232897904L, 10101001233994704L, 10101001231084136L, 10101001233635368L };
         long[] menus = new long[] { 344, 345, 357, 359, 360, 361, 371, 378, 379, 383, 391, 392, 396, 402 };
-        // String ins = "insert into user_menu (site_id, uid, app_id, menu_id, stat,
-        // gmt_create, kind) values (0, %s, 7, 377, 1, now(), 0);\n";
         String sql = "insert into user_menu (site_id, uid, app_id, menu_id, stat, gmt_create, kind) values (0, %s, 7, %s, 1, now(), 0);\n";
         for (long i : uids) {
             for (long j : menus) {
