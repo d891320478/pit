236ed72e109b5b3cb6d906df03040de9ed4aad28
htdong <dht31261@gmail.com>
2022-04-19 21:01:29 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,19 +1,9 @@
-import java.util.TreeSet;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-        TreeSet<Integer> set = new TreeSet<Integer>();
-        set.add(1);
-        set.add(2);
-        set.add(5);
-        set.add(6);
-        System.out.println(set.lower(5));
-        System.out.println(set.floor(4));
-        System.out.println(set.floor(5));
-        System.out.println(set.ceiling(2));
-        System.out.println(set.ceiling(3));
-        System.out.println(set.higher(2));
+        System.out.println(new Date(139931267606272L));
     }
 
 }
