210685325638af174713d8f92c1ebc3beb159712
htdong <dht31261@gmail.com>
2022-05-18 16:52:27 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,17 +1,6 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        File f = new File("/Users/dht31261/Desktop/123.txt");
-        try (FileWriter write = new FileWriter(f)) {
-            for (;;) {
-                write.write("11234567890987654321234567890987654321");
-                write.flush();
-            }
-        } catch (IOException e) {
-        }
+        System.out.println(System.currentTimeMillis());
     }
 }
