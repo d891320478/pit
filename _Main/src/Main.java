b9e2785d4e0eba5836d2f80a7fd603e2cd364779
htdong <dht31261@gmail.com>
2022-05-27 11:59:00 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,6 +1,13 @@
+import java.io.IOException;
+
+/**
+ * 
+ * @author dht31261
+ * @date 2022/05/27
+ */
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws IOException {
         System.out.println(System.currentTimeMillis());
     }
 }
