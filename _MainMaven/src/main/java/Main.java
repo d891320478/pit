687a6223bc56810a1dd0d9fb4875d4c0f4258d12
htdong <dht31261@gmail.com>
2022-05-27 14:15:57 +0800

    c

--- _MainMaven/src/main/java/Main.java
+++ _MainMaven/src/main/java/Main.java
@@ -1,8 +1,10 @@
-import com.shinemo.client.util.AESUtil;
+import java.nio.charset.Charset;
+
+import org.apache.commons.codec.binary.Base64;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.err.println(AESUtil.generateSeed());
+        System.err.println(new String(new Base64().decode("PHA+5ZOI5ZOI5ZOIPC9wPg=="), Charset.forName("utf-8")));
     }
 }
