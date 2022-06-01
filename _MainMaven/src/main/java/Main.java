d74d658025ad5b098e9c17c162bd89a462709ac6
htdong <dht31261@gmail.com>
2022-06-01 21:51:10 +0800

    c

--- _MainMaven/src/main/java/Main.java
+++ _MainMaven/src/main/java/Main.java
@@ -1,10 +1,11 @@
-import java.nio.charset.Charset;
-
-import org.apache.commons.codec.binary.Base64;
+import com.shinemo.client.util.AESUtil;
 
 public class Main {
+    public static final String AES_SEED = "fd614444395d4977a58f5302cec6feb8";
 
     public static void main(String[] args) {
-        System.err.println(new String(new Base64().decode("PHA+5ZOI5ZOI5ZOIPC9wPg=="), Charset.forName("utf-8")));
+        System.out.println(AESUtil.encrypt("3e74c9ffbb674214aa82425183cfb370", AES_SEED));
+        System.out.println(AESUtil.encrypt("VYOHUNS88BGQ8SYNXBVQ", AES_SEED));
+        System.out.println(AESUtil.encrypt("jIKlM9llqSGYgDQZYkarzjkMs37S7kStXNiFeh6U", AES_SEED));
     }
 }
