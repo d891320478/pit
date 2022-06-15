a1bf2caccf21a4cb55be16f04b20ec736b3537e0
htdong <dht31261@gmail.com>
2022-06-15 20:47:25 +0800

    c

--- _MainMaven/src/main/java/Main.java
+++ _MainMaven/src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static final String AES_SEED = "fd614444395d4977a58f5302cec6feb8";
 
     public static void main(String[] args) {
+        System.out.println(AESUtil.encrypt("3e74c9ffbb674214aa82425183cfb370", AES_SEED));
         System.out.println(AESUtil.encrypt("VYOHUNS88BGQ8SYNXBVQ", AES_SEED));
         System.out.println(AESUtil.encrypt("jIKlM9llqSGYgDQZYkarzjkMs37S7kStXNiFeh6U", AES_SEED));
     }
