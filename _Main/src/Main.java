ed7c01ac9ef7019e851c5649afb72da97c9bf2b4
htdong <dht31261@gmail.com>
2022-06-27 15:43:54 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,14 +1,7 @@
-import java.io.File;
-import java.net.URLEncoder;
-
 /**
  * 
  * @author dht31261
  * @date 2022/05/27
  */
 public class Main {
-
-    public static void main(String[] args) {
-        System.err.println(URLEncoder.encode("企管后台-服务器端"));
-    }
 }
