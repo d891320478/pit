78c7c11d2ae4ea6e40fad00b5f8ae1dd721e07e7
htdong <dht31261@gmail.com>
2022-08-21 21:05:43 +0800

    c

--- _Main/src/CaMenu.java
+++ _Main/src/CaMenu.java
@@ -5,9 +5,9 @@ public class CaMenu {
 
     public static void main(String[] args) {
         long[] baasUids = new long[] {8889006639L, 8889006317L, 8889006313L, 8889006320L, 8889006312L, 8889006304L,
-            8889006299L, 8889006711L, 8889006791L, 8889006241L, 8889006242L, 8889006244L, 8889006250L, 8889006467L,
-            8889006677L, 8889006741L, 8889006742L, 8889006251L, 8889006249L, 8889006789L, 8889006844L};
-        long[] menus = new long[] {386L};
+            8889006299L, 8889006711L, 8889006791L, 8889006241L, 8889006242L, 8889006244L, 8889006250L, 8889006677L,
+            8889006741L, 8889006742L, 8889006251L, 8889006249L, 8889006789L, 8889006844L};
+        long[] menus = new long[] {354L};
         String sql =
             "insert into user_menu (site_id, app_id, menu_id, stat, gmt_create, kind, baas_uid) values (0, 7, %s, 1, now(), 0, %s);\n";
         for (int x = 0; x < baasUids.length; ++x) {
--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,36 +1,26 @@
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Main {
 
-    public static Object lock = new Object();
+    public static void main(String[] args) throws IOException {
+        String urlStr = "https://taobao.com";
+        URL url = new URL(urlStr);
+        HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+        connection.connect();
 
-    public static void f1() {
-        synchronized (lock) {
-            try {
-                lock.wait();
-            } catch (InterruptedException e) {
-            }
-            System.out.println("f1");
+        for (Certificate certificate : connection.getServerCertificates()) {
+            X509Certificate x509Certificate = (X509Certificate)certificate;
+            Date stopDate = x509Certificate.getNotAfter();
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            System.out.println(format.format(stopDate));
+            break;
         }
-    }
-
-    public static void f2() {
-        synchronized (lock) {
-            lock.notify();
-            System.out.println("f2");
-        }
-    }
-    // count_over_time(checkStatus{appName="czt_openapi", ip="cztapp.scdzzw.cn", job="checkStatus", result="fail"}[5m])
-
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        // Thread t1 = new Thread(() -> f1());
-        // Thread t2 = new Thread(() -> f2());
-        // t2.start();
-        // t1.start();
-        // Thread.sleep(10000);
-        System.out.print(URLEncoder.encode("https://www.urlencoder.org/", Charset.defaultCharset()));
     }
 }
