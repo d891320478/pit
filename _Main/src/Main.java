4840f7ed32113716aabe69457d5d1b89041fe184
htdong <dht31261@gmail.com>
2022-08-24 16:09:25 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,26 +1,11 @@
+import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.security.cert.Certificate;
-import java.security.cert.X509Certificate;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String urlStr = "https://taobao.com";
-        URL url = new URL(urlStr);
-        HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
-        connection.connect();
-
-        for (Certificate certificate : connection.getServerCertificates()) {
-            X509Certificate x509Certificate = (X509Certificate)certificate;
-            Date stopDate = x509Certificate.getNotAfter();
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            System.out.println(format.format(stopDate));
-            break;
-        }
+        String p = "/Users/dht31261/gitsrc/middleware/dev-ops/pom.xml";
+        File f = new File(p);
+        System.out.println(f.getAbsolutePath());
     }
 }
