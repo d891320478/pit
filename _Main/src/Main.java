ccb047793a2e31d2dfc2664e86c67d8d4d7a8320
htdong <dht31261@gmail.com>
2022-09-27 15:23:09 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,16 +1,11 @@
+
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(URLEncoder.encode(
-            "/home/admin/watchdog/bin/statics_rsync.sh /tmp/webapps/nanjing-yangzi-network-manage-web-0.0.2-noarch.tgz",
-            Charset.defaultCharset()));
-        System.out.println(URLEncoder.encode(
-            "tar xvf /tmp/webapps/nanjing-yangzi-network-manage-web-0.0.2-noarch.tgz -C /", Charset.defaultCharset()));
-        System.out.println(
-            URLEncoder.encode("docker ps -a | grep configproxy", Charset.defaultCharset()));
+        String s = "1.1,2.2";
+        System.out.println(s.replaceAll(",", "|"));
+        System.out.println(System.currentTimeMillis());
     }
 }
