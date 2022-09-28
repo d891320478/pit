3a04523e419ce716a17b585a82db34fa7fafa9ae
htdong <dht31261@gmail.com>
2022-09-28 16:19:39 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,11 +1,10 @@
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String s = "1.1,2.2";
-        System.out.println(s.replaceAll(",", "|"));
-        System.out.println(System.currentTimeMillis());
+        System.out.println(URLEncoder.encode("docker ps -a"));
     }
 }
