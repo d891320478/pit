6982b49ea931bd80196dcc4f2ca175c66cbf0213
htdong <dht31261@gmail.com>
2022-10-31 20:11:11 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -7,6 +7,20 @@ import java.util.Enumeration;
 public class Main {
 
     public static void main(String[] args) throws SocketException {
-        
+        Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (allNetInterfaces.hasMoreElements()) {
+            NetworkInterface net = allNetInterfaces.nextElement();
+            System.out.println(net.getDisplayName());
+            if (net.isLoopback() || net.isVirtual() || !net.isUp()) {
+                continue;
+            }
+            Enumeration<InetAddress> addresses = net.getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                InetAddress ip = addresses.nextElement();
+                if (ip != null && ip instanceof Inet4Address) {
+                    System.out.println(ip.getHostAddress());
+                }
+            }
+        }
     }
 }
