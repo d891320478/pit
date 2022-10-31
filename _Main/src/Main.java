3b6243d3d0c7ef5db49436d9fade5d61eab1e583
htdong <dht31261@gmail.com>
2022-10-31 20:10:22 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,13 +1,12 @@
-import java.net.URLEncoder;
-import java.util.Base64;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(URLEncoder.encode("domain_certificate_expired{url=\"https://saas.uban360.com\"}"));
-        System.out.println(URLEncoder.encode("count_over_time(dockerCheckStatus{state!=\"running\"}[5m])"));
-        System.out.println(new String(Base64.getEncoder().encodeToString(
-            "3+~rx!aT0y>U;2M7[N;g}HAXdWvEH6$Lp@ }~\\dt=0BYp[BOgUBp$e'sCiYz,7#:C#q0X]3T#Z@qQ(o7>#S$M>%POhD'z|M&-*I_|zD8OU`M9ppV(Bupz52#^Li;\\!D@"
-                .getBytes())));
+    public static void main(String[] args) throws SocketException {
+        
     }
 }
