dc724639a7b7232ac87a9eb55e78a7e6031cbab5
htdong <dht31261@gmail.com>
2022-11-29 15:13:31 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,4 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) throws FileNotFoundException {
+        try (Scanner in = new Scanner(new File("/Users/dht31261/Desktop/1.txt"))) {
+            int cnt = 0;
+            while (in.hasNextLine()) {
+                String s = in.nextLine();
+                if (s.contains("aom time = ")) {
+                    cnt += Integer.parseInt(s.substring(s.lastIndexOf("aom time = ") + "aom time = ".length()));
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
 }
