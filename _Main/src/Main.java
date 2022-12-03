faff433327a4a8f1c723d26409d7368a075e7182
htdong <dht31261@gmail.com>
2022-12-03 15:22:00 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,19 +1,8 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.net.URLEncoder;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        try (Scanner in = new Scanner(new File("/Users/dht31261/Desktop/1.txt"))) {
-            int cnt = 0;
-            while (in.hasNextLine()) {
-                String s = in.nextLine();
-                if (s.contains("aom time = ")) {
-                    cnt += Integer.parseInt(s.substring(s.lastIndexOf("aom time = ") + "aom time = ".length()));
-                }
-            }
-            System.out.println(cnt);
-        }
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(URLEncoder.encode("docker ps -a | grep configproxy"));
     }
 }
