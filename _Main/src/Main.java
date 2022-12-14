fffd1a8fdda3b652e51124a238cefdc23b112ee7
htdong <dht31261@gmail.com>
2022-12-14 21:09:52 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -2,7 +2,7 @@ import java.net.URLEncoder;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println(URLEncoder.encode("ls -lh /data/ops/script/interface"));
+    public static void main(String[] args) {
+        System.out.print(URLEncoder.encode("docker restart e37c0f1b678b"));
     }
 }
