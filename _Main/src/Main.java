c1cfb15eb249c94de33b4c377a39f1f4efb60e22
htdong <dht31261@gmail.com>
2022-12-16 09:58:29 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,8 +1,12 @@
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.print(URLEncoder.encode("docker restart e37c0f1b678b"));
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.out.println(URLEncoder.encode("+", "utf-8"));
+        System.out.println(URLDecoder.decode("%2F%20", "utf-8"));
     }
 }
