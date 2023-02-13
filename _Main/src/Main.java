106dbcc565ca76ac87ca59dfff48f209b7d3ec2b
htdong <dht31261@gmail.com>
2023-02-13 19:55:06 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,9 +1,9 @@
+import java.util.Base64;
+
 public class Main {
 
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        System.out.println(sb.toString().length());
-        String[] s = sb.toString().split("-----");
-        System.out.println(s.length);
+        System.out.println(new String(Base64.getUrlDecoder().decode(
+            "OwE4XTpFKL5bdfJXlo4-vV9hNt0g9OKaaDul6ATz5vu_3uK1Sk26Ev2DYIEW_4wd_UsGnFFFK6mZQ54qQ0X1upVhUh3Krc2qLI3UAg==")));
     }
 }
