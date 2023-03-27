d31c6cea2503fd90f721e8970e1709f05420ab2c
htdong <dht31261@gmail.com>
2023-03-27 20:10:24 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,6 +1,10 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println((int)Math.sqrt(100000));
+        System.out.println(
+            LocalDateTime.parse("2020-01-02T23:15:00Z", DateTimeFormatter.ofPattern("YYYY-MM-dd'T'HH:mm:ssZ")));
     }
 }
