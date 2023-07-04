cecceff900467b999ed210325e407b8f32747c03
htdong <dht31261@gmail.com>
2023-07-04 15:09:55 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,8 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(
-            "b6f03f648fe2f726aa3070ab91d531fc7dd90bf83e6ecc1885504ff8e22a6518a7ecc2a8a41538e9f5ec752920cbcd4e2d1d8f7dcc07c99382208c1f4a7f76e75d06946a01d8d5187190919abd9c5743"
-                .length());
+    public static void main(String[] args) throws FileNotFoundException {
+        try (Scanner in = new Scanner(new File("/Users/dht31261/Desktop/1.txt"))) {
+            TreeSet<String> set = new TreeSet<>();
+            while (in.hasNextLine()) {
+                set.add(in.nextLine());
+            }
+            for (String git : set) {
+                System.out.println(git);
+            }
+        }
     }
 }
