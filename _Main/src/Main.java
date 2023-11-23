504d9c242f86cd86beff33a6d539269fcfac6882
htdong <dht31261@gmail.com>
2023-11-23 16:24:21 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,9 +1,4 @@
-import java.io.IOException;
-import java.util.Date;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println(new Date(1695886532787L));
-    }
+    public static void main(String[] args) {}
 }
