52b4eaaef7b804387b89da3e2ed60c4f426750a3
htdong <dht31261@gmail.com>
2024-02-28 17:06:28 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,8 +1,10 @@
-import java.util.Date;
+import java.util.Base64;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new Date(1700737496224L));
+        System.out.print(Base64.getEncoder().encodeToString(
+            "{\"ecName\":\"云南省公路开发投资有限责任公司\",\"apId\":\"JTYJCL\",\"mobiles\":\"15957193120\",\"content\":\"1\",\"sign\":\"h9yS8gOi3\",\"addSerial\":\"\",\"mac\":\"1a4991d8d708424751e889b722e675ed\"}"
+                .getBytes()));
     }
 }
--- _Main/src/com/htdong/leetcode/algorithm/Trie.java
+++ _Main/src/com/htdong/leetcode/algorithm/Trie.java
@@ -3,9 +3,22 @@ package com.htdong.leetcode.algorithm;
 public class Trie {
     public boolean end;
     public Trie[] next;
+    public int len;
 
     public Trie(boolean end, int len) {
         this.end = end;
         this.next = new Trie[len];
+        this.len = len;
+    }
+
+    public static void insert(Trie node, String a) {
+        for (int i = 0; i < a.length(); ++i) {
+            int v = a.charAt(i) - 'a';
+            if(node.next[v] == null) {
+                node.next[v] = new Trie(false, node.len);
+            }
+            node = node.next[v];
+        }
+        node.end = true;
     }
 }
