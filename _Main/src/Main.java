c1d7670c392031331648cce14cba7847ffc43e09
htdong <dht31261@gmail.com>
2025-10-10 11:30:38 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,7 +1,10 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-        char a = 44;
-        System.out.println(a);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日HH时");
+        System.out.println(format.format(new Date()));
     }
 }
