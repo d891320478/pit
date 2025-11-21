d305f1be09ee555ff6e82db69ed14ca9573733ec
htdong <dht31261@gmail.com>
2025-11-21 11:02:23 +0800

    c

--- _Main/src/Main.java
+++ _Main/src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        String[] a = {"bidding:feeds_key:8712c5e3ba5f2af63a57f771b464b08e",
-            "bidding:feeds_key:f1b211ee776f7fe4dc214eff748056a5_total",
-            "bidding:feeds_key:8712c5e3ba5f2af63a57f771b464b08e_total",
-            "bidding:feeds_key:9d47efdfc23b7929594a44f158ffb71f",
-            "bidding:feeds_key:9d47efdfc23b7929594a44f158ffb71f_total",
-            "bidding:feeds_key:f1b211ee776f7fe4dc214eff748056a5"};
-        for (String iter : a) {
-            System.out.println("del " + "\"" + iter + "\"");
-        }
+        String a = "adfa|afadf";
+        String[] b = a.split("\\|");
+        System.out.println(b.length);
+        System.out.println(b[0]);
+        System.out.println(b[1]);
     }
 }
--- _Main/src/com/htdong/leetcode/solution/Solution.java
+++ _Main/src/com/htdong/leetcode/solution/Solution.java
@@ -1,7 +1,7 @@
 package com.htdong.leetcode.solution;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.htdong.leetcode.algorithm.Base;
 
@@ -10,82 +10,84 @@ import com.htdong.leetcode.algorithm.Base;
  * @date 2019年11月7日 下午4:56:49
  */
 public class Solution extends Base {
-    public int maxPathScore(int[][] a, int z) {
-        ++z;
+
+    public static class Pair {
+
+        public int a, b;
+
+        public Pair(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public long getAns() {
+            long n = b - a + 1;
+            return n * (n + 1) / 2;
+        }
+
+        public String toString() {
+            return String.format("{%d, %d}", a, b);
+        }
+    }
+
+    public long[] countStableSubarrays(int[] a, int[][] qu) {
+        List<Pair> list = new ArrayList<>();
+        list.add(new Pair(-1, -1));
         int n = a.length;
-        int m = a[0].length;
-        int[][][] dp = new int[n][m][z];
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                for (int k = 0; k < z; ++k) {
-                    dp[i][j][k] = -1;
+        for (int i = 0; i < n;) {
+            int j = i + 1;
+            for (; j < n;) {
+                if (a[j] >= a[j - 1]) {
+                    ++j;
+                } else {
+                    break;
                 }
             }
+            list.add(new Pair(i, j - 1));
+            i = j;
         }
-        dp[0][0][0] = a[0][0];
-        boolean[][][] vis = new boolean[n][m][z];
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-        Queue<Integer> q3 = new LinkedList<>();
-        q1.add(0);
-        q2.add(0);
-        q3.add(0);
-        while (!q1.isEmpty()) {
-            int x = q1.poll();
-            int y = q2.poll();
-            int w = q3.poll();
-            vis[x][y][w] = false;
-            System.out.printf("%d %d %d %d\n", x, y, w, dp[x][y][w]);
-            if (x + 1 < n) {
-                if (a[x + 1][y] == 0) {
-                    if (dp[x + 1][y][w] < dp[x][y][w]) {
-                        dp[x + 1][y][w] = dp[x][y][w];
-                        if (!vis[x + 1][y][w]) {
-                            vis[x + 1][y][w] = true;
-                            q1.add(x + 1);
-                            q2.add(y);
-                            q3.add(w);
-                        }
-                    }
-                } else if (w + 1 < z) {
-                    if (dp[x + 1][y][w + 1] < dp[x][y][w] + a[x + 1][y]) {
-                        dp[x + 1][y][w + 1] = dp[x][y][w] + a[x + 1][y];
-                        if (!vis[x + 1][y][w + 1]) {
-                            vis[x + 1][y][w + 1] = true;
-                            q1.add(x + 1);
-                            q2.add(y);
-                            q3.add(w + 1);
-                        }
-                    }
-                }
-            }
-            if (y + 1 < m) {
-                if (a[x][y + 1] == 0) {
-                    if (dp[x][y + 1][w] < dp[x][y][w]) {
-                        dp[x][y + 1][w] = dp[x][y][w];
-                        if (!vis[x][y + 1][w]) {
-                            vis[x][y + 1][w] = true;
-                            q1.add(x);
-                            q2.add(y + 1);
-                            q3.add(w);
-                        }
-                    }
-                } else if (w + 1 < z) {
-                    if (dp[x][y + 1][w + 1] < dp[x][y][w] + a[x][y + 1]) {
-                        dp[x][y + 1][w + 1] = dp[x][y][w] + a[x][y + 1];
-                        if (!vis[x][y + 1][w + 1]) {
-                            vis[x][y + 1][w + 1] = true;
-                            q1.add(x);
-                            q2.add(y + 1);
-                            q3.add(w + 1);
-                        }
-                    }
-                }
-            }
+        list.add(new Pair(n + 1, n));
+        long[] b = new long[list.size()];
+        for (int i = 1; i < list.size(); ++i) {
+            b[i] = b[i - 1] + list.get(i).getAns();
         }
-        int ans = -1;
-        for (int i = 0; i < z; ++i) {
-            ans = Math.max(ans, dp[n - 1][m - 1][i]);
+        long[] ans = new long[qu.length];
+        for (int _i = 0; _i < qu.length; ++_i) {
+            int l = 0, r = list.size() - 1;
+            while (l < r) {
+                int mid = l + r >> 1;
+                if (list.get(mid).a < qu[_i][0]) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            }
+            int ll = l;
+            l = 0;
+            r = list.size() - 1;
+            while (l < r) {
+                int mid = l + r + 1 >> 1;
+                if (list.get(mid).b > qu[_i][1]) {
+                    r = mid - 1;
+                } else {
+                    l = mid;
+                }
+            }
+            int rr = r;
+            if (ll <= rr) {
+                ans[_i] = b[rr] - b[ll - 1];
+                if (qu[_i][0] < list.get(ll).a) {
+                    int m = list.get(ll).a - qu[_i][0];
+                    ans[_i] += m * (m + 1) / 2;
+                }
+                if (list.get(rr).b < qu[_i][1]) {
+                    int m = qu[_i][1] - list.get(rr).b;
+                    ans[_i] += m * (m + 1) / 2;
+                }
+            } else {
+                int m = qu[_i][1] - qu[_i][0] + 1;
+                ans[_i] += m * (m + 1) / 2;
+            }
         }
         return ans;
     }
