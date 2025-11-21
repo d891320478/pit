4bf971b70db1b3f56f6cac7581d0f774411f92bc
htdong <dht31261@gmail.com>
2025-11-21 13:44:49 +0800

    c

--- _Main/src/com/htdong/leetcode/solution/LeetCode.java
+++ _Main/src/com/htdong/leetcode/solution/LeetCode.java
@@ -8,7 +8,7 @@ public class LeetCode {
 
     public static void main(String[] args) {
         long t1 = System.currentTimeMillis();
-        System.out.println(new Solution().countStableSubarrays(new int[] {1, 2, 3, 4}, new int[][] {{2, 2}, {3, 3}}));
+        System.out.println(new Solution().countPalindromicSubsequence("aabca"));
         System.out.println("time = " + (System.currentTimeMillis() - t1));
     }
 }
--- _Main/src/com/htdong/leetcode/solution/Solution.java
+++ _Main/src/com/htdong/leetcode/solution/Solution.java
@@ -1,7 +1,9 @@
 package com.htdong.leetcode.solution;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.htdong.leetcode.algorithm.Base;
 
@@ -11,84 +13,25 @@ import com.htdong.leetcode.algorithm.Base;
  */
 public class Solution extends Base {
 
-    public static class Pair {
-
-        public int a, b;
-
-        public Pair(int a, int b) {
-            this.a = a;
-            this.b = b;
+    public int countPalindromicSubsequence(String s) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < 26; ++i) {
+            list.add(new ArrayList<>());
         }
-
-        public long getAns() {
-            long n = b - a + 1;
-            return n * (n + 1) / 2;
+        for (int i = 0; i < s.length(); ++i) {
+            list.get(s.charAt(i) - 'a').add(i);
         }
-
-        public String toString() {
-            return String.format("{%d, %d}", a, b);
-        }
-    }
-
-    public long[] countStableSubarrays(int[] a, int[][] qu) {
-        List<Pair> list = new ArrayList<>();
-        list.add(new Pair(-1, -1));
-        int n = a.length;
-        for (int i = 0; i < n;) {
-            int j = i + 1;
-            for (; j < n;) {
-                if (a[j] >= a[j - 1]) {
-                    ++j;
-                } else {
-                    break;
+        Set<String> set = new HashSet<>();
+        for (int i = 1; i + 1 < s.length(); ++i) {
+            char ch = s.charAt(i);
+            for (int j = 0; j < list.size(); ++j) {
+                if (!list.get(j).isEmpty() && list.get(j).get(0) < i && list.get(j).getLast() > i) {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append((char)('a' + j)).append(ch).append((char)('a' + j));
+                    set.add(sb.toString());
                 }
             }
-            list.add(new Pair(i, j - 1));
-            i = j;
         }
-        list.add(new Pair(n + 1, n));
-        long[] b = new long[list.size()];
-        for (int i = 1; i < list.size(); ++i) {
-            b[i] = b[i - 1] + list.get(i).getAns();
-        }
-        long[] ans = new long[qu.length];
-        for (int _i = 0; _i < qu.length; ++_i) {
-            int l = 0, r = list.size() - 1;
-            while (l < r) {
-                int mid = l + r >> 1;
-                if (list.get(mid).a < qu[_i][0]) {
-                    l = mid + 1;
-                } else {
-                    r = mid;
-                }
-            }
-            int ll = l;
-            l = 0;
-            r = list.size() - 1;
-            while (l < r) {
-                int mid = l + r + 1 >> 1;
-                if (list.get(mid).b > qu[_i][1]) {
-                    r = mid - 1;
-                } else {
-                    l = mid;
-                }
-            }
-            int rr = r;
-            if (ll <= rr) {
-                ans[_i] = b[rr] - b[ll - 1];
-                if (qu[_i][0] < list.get(ll).a) {
-                    int m = list.get(ll).a - qu[_i][0];
-                    ans[_i] += m * (m + 1) / 2;
-                }
-                if (list.get(rr).b < qu[_i][1]) {
-                    int m = qu[_i][1] - list.get(rr).b;
-                    ans[_i] += m * (m + 1) / 2;
-                }
-            } else {
-                int m = qu[_i][1] - qu[_i][0] + 1;
-                ans[_i] += m * (m + 1) / 2;
-            }
-        }
-        return ans;
+        return set.size();
     }
 }
